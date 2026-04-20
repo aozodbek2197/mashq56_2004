504149e64f66828199fbaa2c7944f0600f499bc9
aozodbek2197 <aozodbek2197@gmail.com>
2026-04-20 23:37:04 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+# 1-mashq
+def count_primes(n):
+    if n <= 2: return 0
+    is_prime = [True] * n
+    is_prime[0] = is_prime[1] = False
+    for i in range(2, int(n**0.5) + 1):
+        if is_prime[i]:
+            for j in range(i*i, n, i):
+                is_prime[j] = False
+    return sum(is_prime)
+
+print(count_primes(10))
+# 2-mashq
+def spiral_order(matrix):
+    if not matrix or not matrix[0]: return []
+    result = []
+    top, bottom = 0, len(matrix) - 1
+    left, right = 0, len(matrix[0]) - 1
+    while top <= bottom and left <= right:
+        for i in range(left, right + 1):
+            result.append(matrix[top][i])
+        top += 1
+        for i in range(top, bottom + 1):
+            result.append(matrix[i][right])
+        right -= 1
+        if top <= bottom:
+            for i in range(right, left - 1, -1):
+                result.append(matrix[bottom][i])
+            bottom -= 1
+        if left <= right:
+            for i in range(bottom, top - 1, -1):
+                result.append(matrix[i][left])
+            left += 1
+    return result
+
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+print(spiral_order(matrix))
